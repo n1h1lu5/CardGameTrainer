2c2fcfe2bc97d7612527095a8b4a4e64c9927049
gcroteau <guillaume.croteau.1@gmail.com>
2015-10-25 10:32:32 -0400

    Basic navigation to the blackjack game. The activities are in immersive mode.

--- CardGameTrainerAndroid/app/src/main/java/ca/gcroteau/cardgametrainer/app/Blackjack.java
+++ CardGameTrainerAndroid/app/src/main/java/ca/gcroteau/cardgametrainer/app/Blackjack.java
@@ -1,18 +1,18 @@
 package ca.gcroteau.cardgametrainer.app;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class HomeScreen extends Activity {
+public class Blackjack extends Activity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_home_screen);
-        View homeScreen = findViewById(R.id.home_screen);
+        setContentView(R.layout.activity_blackjack_screen);
+
+        View homeScreen = findViewById(R.id.blackjack_screen);
         homeScreen.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
@@ -20,14 +20,4 @@ public class HomeScreen extends Activity {
                 | View.SYSTEM_UI_FLAG_FULLSCREEN
                 | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
-
-    public void startBlackjack(View view) {
-        Intent intent = new Intent(this, Blackjack.class);
-        startActivity(intent);
-    }
-
-    public void displayAbout(View view) {
-
-    }
-
 }
